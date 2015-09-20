f364ce4e96008167a51a2c39c49400c5ca1d2e78
amyedyentsov <myedyentsov@gmail.com>
2015-09-20 21:58:41 +0300

    fixes

--- hillel/src/AboutMyself.java
+++ hillel/src/AboutMyself.java
@@ -5,22 +5,22 @@ public class AboutMyself {
         Scanner scanner = new Scanner(System.in);
         char space = ' ';
 
-        System.out.println ("Enter Your FirsName: ");
+        System.out.println ("Enter Your First Name: ");
         String firstName = scanner.nextLine();
         firstName = firstName.replace(" ", "");
 
-        System.out.println ("Enter Your LastName: ");
+        System.out.println ("Enter Your Last Name: ");
         String lastName = scanner.nextLine();
         lastName = lastName.replace(" ", "");
 
-        System.out.println ("Please Enter Your Birth Year: ");
+        System.out.println ("Enter Your Birth Year: ");
         int dob = scanner.nextInt();
 
-        System.out.println ("Please Enter Your Birth Year: ");
+        System.out.println ("Enter The Current Year: ");
         int currentDate = scanner.nextInt();
 
         System.out.println ("Your Name Is: " + firstName + " " + lastName);
         System.out.println ("Your Name Is: " + firstName + space + lastName);
-        System.out.println ("And Your age Is: " + (currentDate - dob));
+        System.out.println ("And Your age: " + (currentDate - dob));
     }
 }
