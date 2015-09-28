dc2713068c0650e3b68b33991d2eb7914202f97f
amyedyentsov <myedyentsov@gmail.com>
2015-09-28 03:05:48 +0300

    Homework 2.2

---
+++ hillel/src/Homework/Home.java
@@ -0,0 +1,24 @@
+package Homework;
+
+import java.util.Scanner;
+
+/**
+ * Created by User on 27.09.2015.
+ */
+public class Home {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int outNum;
+
+        System.out.println("Enter the number");
+        int num = scanner.nextInt();
+        outNum = num % 2;
+
+        if (outNum == 0) {
+            System.out.println("Your number is even");
+        } else {
+            System.out.println("your number is odd");
+        }
+    }
+}
