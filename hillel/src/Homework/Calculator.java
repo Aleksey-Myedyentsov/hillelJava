7c40bb962164b1f1c7264438dcc94a1e2da3538d
amyedyentsov <myedyentsov@gmail.com>
2015-10-01 18:11:54 +0300

    HW3

---
+++ hillel/src/Homework/Calculator.java
@@ -0,0 +1,56 @@
+package Homework;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ * Created by User on 30.09.2015.
+ */
+public class Calculator {
+    public static void main(String[] args) {
+        double res = 0.0;
+
+        Locale.setDefault(Locale.US);
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the first number:");
+        double firstNum = scanner.nextDouble();
+
+        System.out.println("Enter a sign of operation:");
+        String operation = scanner.next();
+
+        if (operation.equals("trunk")) {
+            int resTrunk = (int) firstNum;
+            System.out.println(resTrunk);
+        } else {
+            if (operation.equals("fract")) {
+                double resFract = firstNum % 1;
+                System.out.println(resFract);
+            } else {
+
+                System.out.println("Enter the second number:");
+                double secondNum = scanner.nextDouble();
+
+                switch (operation) {
+                    case "+":
+                        res = firstNum + secondNum;
+                        break;
+                    case "-":
+                        res = firstNum - secondNum;
+                        break;
+                    case "*":
+                        res = firstNum * secondNum;
+                        break;
+                    case "/":
+                        res = firstNum / secondNum;
+                        break;
+                    default:
+                        System.out.println("An incorrect value");
+                        break;
+                }
+                System.out.println(res);
+            }
+        }
+    }
+}
