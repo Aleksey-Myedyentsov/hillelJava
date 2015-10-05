33b19955b5d0aa7194307c069f191bf24515fbdb
amyedyentsov <myedyentsov@gmail.com>
2015-10-05 18:12:19 +0300

    HW4

---
+++ hillel/src/Homework/GuessTheNumber.java
@@ -0,0 +1,32 @@
+package Homework;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * Created by User on 04.10.2015.
+ */
+public class GuessTheNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int randNum = random.nextInt(11);
+        int i = 1;
+
+        System.out.println("Enter the number: ");
+        int number = scanner.nextInt();
+
+        do {
+            if (randNum < number) {
+                System.out.println("Your number > randomnumber, enter your number again");
+                number = scanner.nextInt();
+            } else if (randNum > number) {
+                System.out.println("Your number < randomnumber, enter your number again");
+                number = scanner.nextInt();
+            }
+            i++;
+        } while (!(randNum == number));
+
+        System.out.println("You guessed on the " + i + " attempt");
+    }
+}
---
+++ hillel/src/Homework/Telegraph.java
@@ -0,0 +1,25 @@
+package Homework;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ * Created by User on 04.10.2015.
+ */
+public class Telegraph {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter price one word:");
+        double price = scanner.nextDouble();
+
+        System.out.println("Enter the text of message:");
+        String message = scanner.nextLine();
+        //String message = "One Two";
+        String words[] = message.split(" ");
+
+        System.out.println("You need to pay: " + (words.length * price) + ", your message contains " + words.length + " word(s)");
+    }
+}
+
