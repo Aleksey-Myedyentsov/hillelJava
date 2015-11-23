ff42188b1921063c38f86a0f0424383dc77b42bd
amyedyentsov <myedyentsov@gmail.com>
2015-11-23 12:43:19 +0200

    HW_copyFile

--- hillel/src/Homework/MeasurementSpeedCopy.java
+++ hillel/src/Homework/MeasurementSpeedCopy.java
@@ -1,26 +1,51 @@
 package Homework;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.nio.charset.Charset;
 
 /**
  * Created by User on 21.11.2015.
  */
 public class MeasurementSpeedCopy {
+    public static void main(String[] args) {
+        bufferedCopy();
+        streemCopy();
 
-    private static void bufferedRead() {
-        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream("d:/myFile.txt"), Charset.forName("windows-1251")))) {
+    }
+
+    private static void bufferedCopy() {
+        long start = System.currentTimeMillis();
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream("d:/File.txt"), Charset.forName("windows-1251")));
+             BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("d:/copyFile.txt"), Charset.forName("windows-1251")))
+        ) {
             String value = null;
             while ((value = bufferedReader.readLine()) != null) {
-                System.out.println(value);
+                bufferedWriter.write(value + "\n");
             }
-            //throw new IOException("file blabla.txt not found");
+
         } catch (IOException e) {
-            //  System.out.println(e);
             throw new RuntimeException(e);
         }
+        long end = System.currentTimeMillis();
+        long time = end - start;
+        System.out.println("Time stream buffer (mSec): " + time);
     }
+
+    private static void streemCopy() {
+        long start = System.currentTimeMillis();
+        try (InputStream inputStream = new FileInputStream("d:/File.txt");
+             OutputStream outputStream = new FileOutputStream("d:/copyFile1.txt")
+        ) {
+            int value;
+            while ((value = inputStream.read()) != -1) {
+                outputStream.write(value);
+            }
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+        long end = System.currentTimeMillis();
+        long time = end - start;
+        System.out.println("Time stream (mSec): " + (time = end - start));
+    }
+
 }
